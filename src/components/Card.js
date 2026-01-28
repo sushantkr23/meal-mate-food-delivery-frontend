8ccdef2a1d9ec319a38f1d250f2ee5c6d72460e6
Sushant kumar <sushantdehri6@gmail.com>
2026-01-28 19:48:41 +0530

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,72 +2,119 @@ import React, { useEffect, useRef, useState } from 'react'
 import { useDispatchCart, useCart } from './ContextReducer';
 
 export default function Card(props) {
-    let dispatch = useDispatchCart();
-    let data = useCart();
-    const priceRef = useRef();
-    let options = props.options;
-    let priceOptions = Object.keys(options);
-    const [qty, setQty] = useState(1)
-    const [size, setSize] = useState("")
-    const handleAddToCart = async () => {
-        let food = []
-        for (const item of data) {
-            if (item.id === props.foodItem._id) {
-                food = item;
 
-                break;
-            }
-        }
-        if (food !== []) {
-            if (food.size === size) {
-                await dispatch({ type: "UPDATE", id: props.foodItem._id, price: finalPrice, qty: qty })
-                return
-            }
-            else if (food.size !== size) {
-                await dispatch({ type: "ADD", id: props.foodItem._id, name: props.foodItem.name, price: finalPrice, qty: qty, size: size })
-                // await console.log(data)
-                return
-            }
-            return
-        }
-        await dispatch({ type: "ADD", id: props.foodItem._id, name: props.foodItem.name, price: finalPrice, qty: qty, size: size })
-    }
-    let finalPrice = qty * parseInt(options[size]);
+    const dispatch = useDispatchCart();
+    const data = useCart();
+    const priceRef = useRef();
+
+    const options = props.options;
+    const priceOptions = Object.keys(options);
+
+    const [qty, setQty] = useState(1);
+    const [size, setSize] = useState("");
+
     useEffect(() => {
-        setSize(priceRef.current.value)
-    }, [])
+        setSize(priceRef.current.value);
+    }, []);
+
+    const finalPrice = qty * parseInt(options[size]);
+
+    const handleAddToCart = async () => {
+
+        // ✅ Find existing item in cart
+        const food = data.find(item => item.id === props.foodItem._id);
+
+        if (food) {
+            // Same size → UPDATE
+            if (food.size === size) {
+                await dispatch({
+                    type: "UPDATE",
+                    id: props.foodItem._id,
+                    price: finalPrice,
+                    qty: qty
+                });
+                return;
+            }
+            // Different size → ADD new item
+            else {
+                await dispatch({
+                    type: "ADD",
+                    id: props.foodItem._id,
+                    name: props.foodItem.name,
+                    price: finalPrice,
+                    qty: qty,
+                    size: size
+                });
+                return;
+            }
+        }
+
+        // Item not in cart → ADD
+        await dispatch({
+            type: "ADD",
+            id: props.foodItem._id,
+            name: props.foodItem.name,
+            price: finalPrice,
+            qty: qty,
+            size: size
+        });
+    };
 
     return (
         <div>
-            <div>
-                <div className="card mt-3" style={{ width: "18rem", "maxHeight": "360px" }}>
-                    <img src={props.foodItem.img} className="card-img-top" alt="..." style={{ height: "120px", objectFit: "fill" }} />
-                    <div className="card-body">
-                        <h5 className="card-title">{props.foodItem.name}</h5>
+            <div className="card mt-3" style={{ width: "18rem", maxHeight: "360px" }}>
+                <img
+                    src={props.foodItem.img}
+                    className="card-img-top"
+                    alt={props.foodItem.name}
+                    style={{ height: "120px", objectFit: "fill" }}
+                />
 
-                        <div className='container w-100' >
-                            <select className='m-2 h-100 bg-success rounded' onChange={(e) => setQty(e.target.value)}>
-                                {Array.from(Array(6), (e, i) => {
-                                    return (
-                                        <option key={i + 1} value={i + 1}>{i + 1}</option>
-                                    )
-                                })}
-                            </select>
-                            <select className='m-2 h-100 bg-success rounded' ref={priceRef} onChange={(e) => setSize(e.target.value)}>
-                                {priceOptions.map((data) => {
-                                    return <option key={data} value={data}>{data}</option>
-                                })}
-                            </select>
-                            <div className='d-inline h-100 fs-5'>
-                                ₹{finalPrice}/-
-                            </div>
+                <div className="card-body">
+                    <h5 className="card-title">{props.foodItem.name}</h5>
+
+                    <div className="container w-100">
+                        {/* Quantity */}
+                        <select
+                            className="m-2 h-100 bg-success rounded"
+                            onChange={(e) => setQty(Number(e.target.value))}
+                        >
+                            {Array.from(Array(6), (_, i) => (
+                                <option key={i + 1} value={i + 1}>
+                                    {i + 1}
+                                </option>
+                            ))}
+                        </select>
+
+                        {/* Size */}
+                        <select
+                            className="m-2 h-100 bg-success rounded"
+                            ref={priceRef}
+                            onChange={(e) => setSize(e.target.value)}
+                        >
+                            {priceOptions.map((option) => (
+                                <option key={option} value={option}>
+                                    {option}
+                                </option>
+                            ))}
+                        </select>
+
+                        {/* Price */}
+                        <div className="d-inline h-100 fs-5">
+                            ₹{finalPrice}/-
                         </div>
-                        <hr>
-                        </hr>
-                        <button className={'btn btn-success justify-center ms-2 '} onClick={handleAddToCart}>Add to Cart</button>
                     </div>
+
+                    <hr />
+
+                    <button
+                        className="btn btn-success ms-2"
+                        onClick={handleAddToCart}
+                    >
+                        Add to Cart
+                    </button>
                 </div>
             </div>
         </div>
-    )
+    );
 }
