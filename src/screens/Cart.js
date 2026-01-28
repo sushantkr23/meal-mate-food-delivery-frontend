8343924a3a785c6e749cf0431ecb9e07234e1d5c
Sushant kumar <sushantdehri6@gmail.com>
2026-01-29 00:08:49 +0530

    Update Cart.js

--- src/screens/Cart.js
+++ src/screens/Cart.js
@@ -17,7 +17,7 @@ export default function Cart() {
     const handleCheckOut = async () => {
         let userEmail = localStorage.getItem("userEmail");
 
-        let response = await fetch("http://localhost:5000/api/orderData", {
+        let response = await fetch("https://meal-mate-food-delivery-backend.onrender.com/api/orderData", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
