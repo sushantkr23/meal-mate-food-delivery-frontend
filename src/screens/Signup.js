5623a972b252a33fff045688f7b020f14b1c1474
Sushant kumar <sushantdehri6@gmail.com>
2026-01-29 00:08:07 +0530

    Update Signup.js

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -7,7 +7,8 @@ export default function Signup() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/api/creatuser", {
+    const response = await fetch("https://meal-mate-food-delivery-backend.onrender.com/api/creatuser
+", {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
